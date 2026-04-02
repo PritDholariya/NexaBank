8fde4eea9b1601a959e7bc562929e3fe07ab72d5
Meet Kalathiya <meetkalathiya012@gmail.com>
2026-04-02 16:35:26 +0200

    Update UI of landing Page

--- frontend/app/(marketing)/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function AboutPage() {
-  return (
-    <main>
-      <h1>About NexaBank</h1>
-      <p>Reference About page inside (marketing) route group.</p>
-    </main>
-  );
-}
--- frontend/app/(marketing)/contact/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function ContactPage() {
-  return (
-    <main>
-      <h1>Contact Us</h1>
-      <p>Reference Contact page inside (marketing) route group.</p>
-    </main>
-  );
-}
